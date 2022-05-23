ce0786c86e7f5af2a8fb5521caea2bee224aeae2
Thiagomatheus12 <thi_ago5@hotmail.com>
2022-05-23 16:37:14 -0300

    update carousel

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+import('.style.css')
+import('.style.scss')
+
 function pesquisar() {
     document.getElementById("campo-de-pesquisa").focus()
     document.getElementById("campo-de-pesquisa").select()
     console.log("Pesquisar")
 }
+
